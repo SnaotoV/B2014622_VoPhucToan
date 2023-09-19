45ad1a307305203c326756e2f08b9d1c7b2106b1
Vo Phuc Toan <vophuctoan365@gmail.com>
2023-09-19 22:13:32 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const expresss = require('express');
 const cors = require('cors');
+const contactsRouter = require("./app/routes/contact.route");
 
 const app = expresss();
 
 app.use(cors());
 app.use(expresss.json());
-
 app.get('/', (req, res) => {
     res.json({ messeage: 'Welcome to contact book application.' });
 });
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
